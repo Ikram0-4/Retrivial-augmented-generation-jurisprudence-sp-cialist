fffd1a6e2b97424005b3b7b0e05449c37a6aff2d
ikram0-4 <100198865+Ikram0-4@users.noreply.github.com>
2026-05-27 10:46:35 +0200

    Format Vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.', // indique que index.html est ici
+  root: '.',
   server: {
-    port: 5173, // optionnel
+    port: 5173,
   },
 })
